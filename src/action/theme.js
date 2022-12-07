6f78dd0055dc1ffaf98c967b515fb936fe24efda
Tushiar <tusharmoolya1234@gmail.com>
2022-12-07 13:59:56 +0530

    changes for api route

--- src/action/theme.js
+++ src/action/theme.js
@@ -4,7 +4,7 @@ import { CLEAR_ALERT, THEME_UPDATED } from "./types";
 //Get Posts
 export const updateTheme = (theme) => async (dispatch) => {
   try {
-    await axios.put("/https://sapiens-service.onrender.com/api/auth/theme", { theme });
+    await axios.put("https://sapiens-service.onrender.com/api/auth/theme", { theme });
     dispatch({
       type: THEME_UPDATED,
       payload: {
